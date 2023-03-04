98c169eb9c039777e9224a2795c2988ec4cc0545
Roch Armengaud <armengaud.roch@live.fr>
2023-03-04 15:34:51 +0100

    Designed and imported own logo for navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,13 @@
 import React from 'react';
+import { FaBars, FaTimes } from 'react-icons/fa';
+import RAlogo from '../assets/RA_logo.png';
 
 export const Navbar = () => {
-  return <div className="fixed w-full h-[80px] flex justify-between items-center px-4 bg-[#0a192f]"></div>;
+  return (
+    <div className="fixed w-full h-[80px] flex justify-between items-center px-4 bg-[#0a192f] text-gray-300">
+      <div>
+        <img src={RAlogo} alt="Website logo" style={{ width: '80px' }}></img>
+      </div>
+    </div>
+  );
 };
