cdcf1e0ade838669be6a9ac795e306604f444986
Roch Armengaud <armengaud.roch@live.fr>
2023-03-06 13:57:38 +0100

    added anchor for work on view work btn

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { HiArrowNarrowRight } from 'react-icons/hi';
+import { Link } from 'react-scroll';
 
 export const Home = () => {
   return (
@@ -23,7 +24,9 @@ export const Home = () => {
         <div>
           {/* The group classname here is to make the rotate-90 effect apply when the user points the button too */}
           <button className="group hover:bg-pink-600 hover:border-pink-600 flex items-center px-6 py-3 my-2 text-white border-2">
-            View Work
+            <Link to="work" smooth={true} duration={500}>
+              Work
+            </Link>
             <span className="group-hover:rotate-90 duration-300">
               <HiArrowNarrowRight className="ml-3" />
             </span>
