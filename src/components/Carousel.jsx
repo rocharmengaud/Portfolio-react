5347b333086d4a9d2aeb02887f877b2b027dc584
Roch Armengaud <armengaud.roch@live.fr>
2023-06-21 17:10:27 +0200

    Fixed sizes for small mobile screens

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -29,37 +29,37 @@ export const Carousel = ({ workItems }) => {
 
   return (
     <>
-      <div className="sm:max-w-[600px] max-w-[300px] sm:h-[600px] h-[500px] w-full relative mx-auto flex flex-col justify-center items-center">
+      <div className="sm:max-w-[600px] max-w-[300px] sm:h-[600px] h-[400px] w-full relative mx-auto flex flex-col justify-center items-center">
         <div
-          style={{ backgroundImage: `url(${workItems[currentIndex].img})` }}
+          style={{ backgroundImage: `url(${workItems[currentIndex].img})`, filter: 'blur(1px)' }}
           className="rounded-2xl w-full h-full bg-left-top bg-cover shadow-lg shadow-[#040c16] relative flex justify-center items-center duration-500"
-        >
-          <div className="sm:gap-12 flex flex-col items-center gap-4">
-            <h1 className="sm:text-4xl text-2xl font-bold text-pink-600">{workItems[currentIndex].name}</h1>
-            <div className="sm:gap-6 flex justify-center gap-2">
-              <a href={workItems[currentIndex].demoURL}>
-                <button className="sm:px-8 sm:py-4 hover:bg-blue-400 hover:border-blue-500 px-3 py-2 font-bold text-white bg-blue-500 border-b-4 border-blue-700 rounded">
-                  Demo
-                </button>
-              </a>
-              <a href={workItems[currentIndex].codeURL}>
-                <button className="sm:px-8 sm:py-4 hover:bg-blue-400 hover:border-blue-500 px-3 py-2 font-bold text-white bg-blue-500 border-b-4 border-blue-700 rounded">
-                  Code
-                </button>
-              </a>
-              <button
-                onClick={toggleModal}
-                className="sm:px-8 sm:py-4 hover:bg-blue-400 hover:border-blue-500 px-3 py-2 font-bold text-white bg-blue-500 border-b-4 border-blue-700 rounded"
-              >
-                Infos
+        ></div>
+        <div className="sm:gap-12 flex flex-col items-center gap-4 absolute top-[40%]">
+          <h1 className="sm:text-4xl text-2xl font-bold text-pink-600">{workItems[currentIndex].name}</h1>
+          <div className="sm:gap-6 flex justify-center gap-2">
+            <a href={workItems[currentIndex].demoURL}>
+              <button className="sm:px-8 sm:py-4 hover:bg-blue-400 hover:border-blue-500 px-3 py-2 font-bold text-white bg-blue-500 border-b-4 border-blue-700 rounded">
+                Demo
               </button>
-            </div>
+            </a>
+            <a href={workItems[currentIndex].codeURL}>
+              <button className="sm:px-8 sm:py-4 hover:bg-blue-400 hover:border-blue-500 px-3 py-2 font-bold text-white bg-blue-500 border-b-4 border-blue-700 rounded">
+                Code
+              </button>
+            </a>
+            <button
+              onClick={toggleModal}
+              className="sm:px-8 sm:py-4 hover:bg-blue-400 hover:border-blue-500 px-3 py-2 font-bold text-white bg-blue-500 border-b-4 border-blue-700 rounded"
+            >
+              Infos
+            </button>
           </div>
         </div>
-        <div className="absolute top-[50%] left-0 sm:left-2 rounded-full sm:p-2 p-1 bg-black/10 text-white cursor-pointer hover:text-pink-600">
+
+        <div className="absolute top-[50%] left-0 sm:left-2 rounded-full sm:p-2 p-1 bg-black/10 text-white cursor-pointer hover:opacity-80">
           <BsChevronCompactLeft size={30} onClick={prevSlide} />
         </div>
-        <div className="absolute top-[50%] right-0 sm:right-2 rounded-full sm:p-2 p-1 bg-black/10 text-white cursor-pointer hover:text-pink-600">
+        <div className="absolute top-[50%] right-0 sm:right-2 rounded-full sm:p-2 p-1 bg-black/10 text-white cursor-pointer hover:opacity-80">
           <BsChevronCompactRight size={30} onClick={nextSlide} />
         </div>
         <div className="bottom-8 absolute flex justify-center w-full">
--- src/components/Skill.jsx
+++ src/components/Skill.jsx
@@ -1,7 +1,7 @@
 export const Skill = ({ icon, name }) => {
   return (
     <div className="shadow-md shadow-[#040c16] hover:scale-110 duration-500">
-      <img className="w-20 mx-auto" src={icon} alt={`${name} icon`} />
+      <img className="w-14 mx-auto" src={icon} alt={`${name} icon`} />
       <p className="my-4">{name}</p>
     </div>
   );
